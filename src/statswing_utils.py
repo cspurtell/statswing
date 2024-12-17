652fa012211210fddac6780b2e3889116d903500
Colin Purtell <cpurtell13@gmail.com>
2024-12-17 17:17:08 -0500

    Update statswing_utils.py

--- src/statswing_utils.py
+++ src/statswing_utils.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from src.config import TEAM_NAME_MAPPING, STAT_MAPPING, STAT_DESCRIPTIONS
 
-def load_data(file_path):
-    """Loads the player data from a CSV file"""
+def load_data(file_path: str) -> pd.DataFrame:
+    '''
+
+    Loads the player data from a CSV file
+    
+    '''
     try:
         data = pd.read_csv(file_path)
         return data
@@ -10,9 +14,19 @@ def load_data(file_path):
         print(f'Error: The file {file_path} could not be found.')
         return
     
-def find_player(data, name):
+def find_player(data: pd.DataFrame, name: str) -> pd.DataFrame:
+    '''
+    
+    Finds a player in the dataset by name
+
+    '''
     return data[data["Name"] == name]
 
-def get_dataset_column(stat_name):
+def get_dataset_column(stat_name: str) -> str:
+    '''
+    
+    Retrieves the corresponding dataset column name for a given stat alias
+    
+    '''
     alias_to_col = {alias: column for column, alias in STAT_MAPPING.items()}
     return alias_to_col.get(stat_name, stat_name)
